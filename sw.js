6606970d5c3757d876fdc063dc49db6199a057de
kazuyuki <m.kazuyuki@gmail.com>
2023-09-09 01:48:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+/*
 self.addEventListener("fetch", event => {
   console.log('WORKER: Fetching', event.request);
 });
+*/
+
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
