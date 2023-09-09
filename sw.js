d63cb7ad96d9858f4a174608705881af96e6d483
kazuyuki <m.kazuyuki@gmail.com>
2023-09-09 22:59:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,20 +5,33 @@ self.addEventListener("fetch", event => {
 */
 
 // This is the "Offline copy of pages" service worker
-
 const CACHE = "pwabuilder-offline";
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
-
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
+
+//
+// To be installable
+//
+const CACHE_NAME = 'cool-cache';
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
