b49e550708b0dce602d6e41d19a2a064cce714d6
kazuyuki <m.kazuyuki@gmail.com>
2023-09-13 17:59:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,41 @@
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
+});
+
 /*
 self.addEventListener("install", event => {
   console.log('WORKER: Install', event.request);
@@ -10,6 +48,7 @@ self.addEventListener("fetch", event => {
 });
 */
 
+/*
 // This is the "Offline copy of pages" service worker
 const CACHE = "pwabuilder-offline";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -24,4 +63,4 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
-
+*/
